2b252707446b5ccbaeac8af8318c00c58724a1ef
Tumza89 <tumelomokumo67@gmail.com>
2026-08-10 17:16:20 +0200

    Update server.js

--- public/server.js
+++ public/server.js
@@ -7,7 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, 'public')));
