34976c0a04f8297e4c215c61b2934c1283c7414d
Tumza89 <tumelomokumo67@gmail.com>
2026-08-11 12:39:34 +0200

    Update server.js

--- public/server.js
+++ public/server.js
@@ -6,8 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 app.post('/api/analyze', async (req, res) => {
   try {
     const { image, symbol, tradingMode, highProbMode } = req.body;
